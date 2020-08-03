5859b302238334a039fd2c598a2b286fc60ecd83
ChaostixZix <arip.salawazna@gmail.com>
2020-08-03 20:25:49 +0700

    Save

--- src/sp/task/GameTask.php
+++ src/sp/task/GameTask.php
@@ -9,30 +9,34 @@ use pocketmine\Server;
 
 use sp\Main;
 
-class GameTask extends Task{
-  public $p, $lvl, $time;
+class GameTask extends Task
+{
+    public $p, $lvl, $time;
 
-public function __construct(Main $plugin, string $lvl) {
-$this->p = $plugin;
-$this->lvl = $lvl;
-$this->time = 300;
-}
-public function onRun(int $currentTick) : void {
-$this->time--;
-if(count($this->p->getServer()->getLevelByName($this->lvl)->getPlayers()) <= 1){
-$this->p->getScheduler()->cancelTask($this->getTaskId());
-}
-if($this->time <= 1){
-$this->p->nichya($this->lvl);
+    public function __construct(Main $plugin, string $lvl)
+    {
+        $this->p = $plugin;
+        $this->lvl = $lvl;
+        $this->time = 300;
+    }
 
-} else {
-foreach($this->p->getServer()->getLevelByName($this->lvl)->getPlayers() as $pl) {
-$c = count($this->p->getServer()->getLevelByName($this->lvl)->getPlayers());
-$pl->sendTip(str_replace("{COUNT}", $c, $this->p->getMsg()->get("player_count")));
-}
-$this->p->getWinner($this->lvl);
-$this->p->fix($this->lvl);
+    public function onRun(int $currentTick): void
+    {
+        $this->time--;
+        if (count($this->p->getServer()->getLevelByName($this->lvl)->getPlayers()) <= 1) {
+            $this->p->getScheduler()->cancelTask($this->getTaskId());
+        }
+        if ($this->time <= 1) {
+            $this->p->nichya($this->lvl);
 
-}
-}
+        } else {
+            foreach ($this->p->getServer()->getLevelByName($this->lvl)->getPlayers() as $pl) {
+                $c = count($this->p->getServer()->getLevelByName($this->lvl)->getPlayers());
+                $pl->sendTip(str_replace("{COUNT}", $c, $this->p->getMsg()->get("player_count")));
+            }
+            $this->p->getWinner($this->lvl);
+            $this->p->fix($this->lvl);
+
+        }
+    }
 } 
--- src/sp/task/StartTask.php
+++ src/sp/task/StartTask.php
@@ -9,37 +9,43 @@ use pocketmine\Server;
 
 use sp\Main;
 
-class StartTask extends Task{
-  public $p, $lvl, $time, $start;
+class StartTask extends Task
+{
+    public $p, $lvl, $time, $start;
 
-public function __construct(Main $plugin, string $lvl) {
-$this->p = $plugin;
-$this->lvl = $lvl;
-$this->time = 30;
-$this->start = 0;
-}
-public function onRun(int $currentTick) : void {
-if(count($this->p->getServer()->getLevelByName($this->lvl)->getPlayers()) >= 2){
-$this->time--;
-foreach($this->p->getServer()->getLevelByName($this->lvl)->getPlayers() as $pl) {
-$pl->sendTip(str_replace("{COUNT}", $this->time, $this->p->getMsg()->get("timer_start")));
-if($this->time <= 0){
-$this->p->setGame($this->lvl, true);
-$this->startg();
+    public function __construct(Main $plugin, string $lvl)
+    {
+        $this->p = $plugin;
+        $this->lvl = $lvl;
+        $this->time = 30;
+        $this->start = 0;
+    }
 
-$this->p->getScheduler()->cancelTask($this->getTaskId());
+    public function onRun(int $currentTick): void
+    {
+        if (count($this->p->getServer()->getLevelByName($this->lvl)->getPlayers()) >= 2) {
+            $this->time--;
+            foreach ($this->p->getServer()->getLevelByName($this->lvl)->getPlayers() as $pl) {
+                $pl->sendTip(str_replace("{COUNT}", $this->time, $this->p->getMsg()->get("timer_start")));
+                if ($this->time <= 0) {
+                    $this->p->setGame($this->lvl, true);
+                    $this->startg();
 
-}
-}
+                    $this->p->getScheduler()->cancelTask($this->getTaskId());
 
-} else {
-$this->p->getScheduler()->cancelTask($this->getTaskId());
-}
-}
-public function startg() : void {
-if($this->start == 0){
-$this->start++;
-$this->p->startGame($this->lvl);
-}
-}
+                }
+            }
+
+        } else {
+            $this->p->getScheduler()->cancelTask($this->getTaskId());
+        }
+    }
+
+    public function startg(): void
+    {
+        if ($this->start == 0) {
+            $this->start++;
+            $this->p->startGame($this->lvl);
+        }
+    }
 }
